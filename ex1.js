a8a01cf73ad421736363f504d1b7afb0a98f83f0
gegebe <ggirbes@gmail.com>
2023-10-18 09:01:16 +0200

    object 1

--- ex1.js
+++ ex1.js
@@ -6,6 +6,8 @@ let vacuna = {
   efectividad: "95",
 };
 
+console.log(vacuna.efectividad);
+
 /**
  * Escribe una setencia que muestre por consola la efectividad de la vacuna. SOLO debe mostrar el valor de la propiedad 'efectividad'
  *
